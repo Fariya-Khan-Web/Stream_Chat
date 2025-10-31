c92ce0c3e229c08648ff85d8b9578d567e0bbf63
Fariya <fariya.webdev@gmail.com>
2025-10-31 20:19:39 +0600

    feat: accept request api

--- backend/src/Controller/user-controller.js
+++ backend/src/Controller/user-controller.js
@@ -53,16 +53,46 @@ export const sendFriendReq = async (req, res) => {
             return res.status(400).json({ message: "A friend request already exists between you and this user" })
         }
 
-    
-        const friendRequest = FriendRequest.create({
+
+        const friendRequest = await FriendRequest.create({
             sender: myId,
             recipient: recipientID
         })
         res.status(200).json(friendRequest)
-    
-    
+
+
     } catch (error) {
         console.log("Error in sendFriendReq controller:", error)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+
+export const acceptRequest = async (req, res) => {
+    try {
+        const myId = req.user.id
+        const { id } = req.params
+
+        const request = await FriendRequest.findById(id)
+        if (!request) return res.status(404).json({ message: "Friend request not found" })
+
+        if (request.recipient.toString() !== myId) {
+            res.status(403).json({ message: "User not authorized to accept this request" })
+        }
+
+        request.status = "accepted"
+        await request.save()
+
+        // adding each other to their friend list
+        await User.findByIdAndUpdate(request.sender, {
+            $addToSet: {friends: request.recipient} // only add if it doesn't exist already
+        })
+        await User.findByIdAndUpdate(request.recipient, {
+            $addToSet: {friends: request.sender}
+        })
+
+    } catch (error) {
+        console.log("Error in acceptRequest controller:", error)
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
