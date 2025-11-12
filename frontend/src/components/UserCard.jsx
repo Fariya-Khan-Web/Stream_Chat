e3cfd76f56eaa3485ed01c144c577fe3c8df440b
Fariya <fariya.webdev@gmail.com>
2025-11-12 23:33:43 +0600

    user card

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -1,9 +1,30 @@
 import { MapPin } from 'lucide-react';
 import React from 'react';
+import { LANGUAGE_TO_FLAG } from '../constants';
 
 const UserCard = ({ user, }) => {
 
-    const { fullName, profilePic, nativeLanguage, learningLanguage, bio , location} = user
+    const { fullName, profilePic, nativeLanguage, learningLanguage, bio, location } = user
+    console.log({ user })
+
+    const getLanguageFlag = (language) => {
+        if (!language) return null;
+
+        const langLower = language.toLowerCase();
+        const countryCode = LANGUAGE_TO_FLAG[langLower];
+
+        if (countryCode) {
+            console.log({ countryCode })
+            return (
+                <img
+                    src={`https://flagcdn.com/24x18/${countryCode}.png`}
+                    alt={`${langLower} flag`}
+                    className="h-3 mr-1 inline-block"
+                />
+            );
+        }
+        return null;
+    }
 
     return (
         <div className='p-4 rounded-xl bg-base-300/80'>
@@ -11,15 +32,36 @@ const UserCard = ({ user, }) => {
                 <img src={profilePic} className='size-13' alt="profile picture" />
                 <div>
                     <h2 className='font-semibold text-lg'>{fullName}</h2>
-                    <p className='text-sm flex items-center gap-1 '><MapPin className='size-4'/>{location}</p>
+                    <p className='text-sm flex items-center gap-1 '><MapPin className='size-4' />{location}</p>
                 </div>
             </div>
             <div className='space-x-2 my-4'>
-                <span className='capitalize px-2 bg-primary rounded-full'>Native: {nativeLanguage}</span>
-                <span className='capitalize px-1.5 rounded-full border'>Learning: {learningLanguage}</span>
+                <span className='capitalize px-2.5 pb-1 bg-primary rounded-full'>{getLanguageFlag(nativeLanguage)}Native: {nativeLanguage}</span>
+                <span className='capitalize px-2 pb-1 rounded-full border'>{getLanguageFlag(learningLanguage)}Learning: {learningLanguage}</span>
             </div>
 
             <p>{bio}</p>
+
+
+            {/* Action button */}
+            <button
+                className={`btn w-full mt-2 ${hasRequestBeenSent ? "btn-disabled" : "btn-primary"
+                    } `}
+                onClick={() => sendRequestMutation(user._id)}
+                disabled={hasRequestBeenSent || isPending}
+            >
+                {hasRequestBeenSent ? (
+                    <>
+                        <CheckCircleIcon className="size-4 mr-2" />
+                        Request Sent
+                    </>
+                ) : (
+                    <>
+                        <UserPlusIcon className="size-4 mr-2" />
+                        Send Friend Request
+                    </>
+                )}
+            </button>
 
 
 
