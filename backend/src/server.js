2059b85b9689b8b6735df895ade04aeb113a0830
Fariya <fariya.webdev@gmail.com>
2025-11-17 04:21:13 +0600

    Fixed node_modules issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+dotenv.config()
 import cors from 'cors'
 import cookieParser from "cookie-parser"
+import path from 'path'
+ 
 import authRoutes from './Routes/auth-routes.js'
 import userRoutes from './Routes/user-routes.js'
 import chatRoutes from './Routes/chat-routes.js'
+
 import { connectDB } from './lib/db.js'
 
 const app = express()
-dotenv.config()
 const PORT = process.env.PORT
+
+const _dirname = path.resolve()
 
 
 app.use(
@@ -26,9 +31,14 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/chats', chatRoutes)
 
-app.get('/', (req, res) => {
-    res.send('wellcome')
-})
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`)
